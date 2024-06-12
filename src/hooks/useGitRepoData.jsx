40ec2991e74ab1e6036dfc12e2074a0225a32091
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-06-12 19:51:20 +0600

    Issue FIx

--- src/hooks/useGitRepoData.jsx
+++ src/hooks/useGitRepoData.jsx
@@ -13,16 +13,16 @@ const useGitRepoData = (username) => {
     queryKey: ["gitRepoData"],
     enabled: !!user,
     queryFn: async () => {
-      // const res = await axios.get(
-      //   `https://api.github.com/users/${username}/repos?page=1&per_page=99&clientId=${
-      //     import.meta.env.VITE_clientID
-      //   }&clientSecret=${import.meta.env.VITE_clientSecret}`,
-      //   {
-      //     auth: import.meta.env.VITE_GITHUB_USERNAME,
-      //     password: import.meta.env.VITE_GITHUB_OLD_TOKEN,
-      //   }
-      // );
-      // return res.data;
+      const res = await axios.get(
+        `https://api.github.com/users/${username}/repos?page=1&per_page=99&clientId=${
+          import.meta.env.VITE_clientID
+        }&clientSecret=${import.meta.env.VITE_clientSecret}`,
+        {
+          auth: import.meta.env.VITE_GITHUB_USERNAME,
+          password: import.meta.env.VITE_GITHUB_OLD_TOKEN,
+        }
+      );
+      return res.data;
     },
     onError: (err) => {
       console.error("Error fetching GitHub repositories:", err);
